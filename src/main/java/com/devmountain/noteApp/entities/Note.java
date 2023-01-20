f2a5f24e737400b3e90e4746de82d7c62f84b00b
Veronica Osei <veronicaosei273@gmail.com>
2023-01-19 18:24:22 -0600

    complete

---
+++ src/main/java/com/devmountain/noteApp/entities/Note.java
@@ -0,0 +1,31 @@
+package com.devmountain.noteApp.entities;
+
+import com.devmountain.noteApp.dtos.NoteDto;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "Notes")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Note {
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "text")
+    private String body;
+
+    @ManyToOne
+    @JsonBackReference
+    private User user;
+
+    public Note(NoteDto noteDto) {
+    }
+}
